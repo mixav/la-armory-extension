e45e0e966bbbe3a61cb7a47477b4cdd76e21e2e3
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2022-12-16 00:09:43 +0300

    add engraving mod

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,5 +1,6 @@
 let ratingByClass = {};
 let characterList = [];
+const negativeList = ['Износ', 'Ржавчина', 'Медлительность', 'Миролюбие'];
 
 function getForClasses(className) {
     chrome.runtime.sendMessage({type: 'load-for-class', message: className}, (response) => {
@@ -40,3 +41,31 @@ button.onclick = function () {
 button.innerHTML = '&circlearrowright;'
 expandCharacterList.prepend(button);
 getForClasses([...new Set(characterList.map(item => item.class))]);
+
+function modifyEngravingsList() {
+    let allEngravingList = document.querySelectorAll('.swiper-slide>li');
+    if (allEngravingList.length > 0) {
+        allEngravingList.forEach(element => {
+            let divStyled = document.createElement('div')
+            divStyled.style.justifyContent = "space-between"
+            divStyled.innerHTML = element.firstElementChild.innerHTML.replace(/(.+)\s(\d ур\.)/g, '<span>$1</span><span>$2</span>')
+            if (negativeList.includes(divStyled.firstElementChild.innerText)) {
+                divStyled.firstElementChild.style.color = 'red'
+            }
+
+            element.firstElementChild.replaceWith(divStyled)
+        })
+        if (allEngravingList.length > 3) {
+            let engravingElement = document.querySelector('ul.swiper-slide.swiper-slide-active');
+            let engravingsSwiper = document.querySelector('div.swiper-wrapper');
+            if (engravingElement) {
+                engravingElement.replaceChildren(...allEngravingList);
+                document.querySelector('.swiper-option').remove();
+                document.querySelector('.profile-ability-engrave').style.height = 'auto';
+                engravingsSwiper.replaceChildren(document.querySelector('div.swiper-wrapper').firstElementChild)
+            }
+        }
+    }
+}
+
+modifyEngravingsList();
