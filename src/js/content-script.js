d5de9568b6d226b2a237962dd8dd2ecc65508fc7
Mikhail Viktarenkau <mihaviktorenkov@gmail.com>
2022-12-24 15:04:25 +0300

    add quality display feature

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,9 +1,10 @@
 let ratingByClass = {};
 let characterList = [];
 const negativeList = ['Износ', 'Ржавчина', 'Медлительность', 'Миролюбие'];
+import {COLOR_TABLE} from "../constants/ColorTable";
+
 const settings = {
-    loadAtStart: true,
-    modifyEngravings: true
+    loadAtStart: true, modifyEngravings: true
 }
 
 const loadAllCharacters = function () {
@@ -84,6 +85,59 @@ function modifyEngravingsList() {
         }
     }
 }
+
+function displayQuality() {
+    let profileData = {};
+    let equipmentList = document.querySelectorAll('.profile-equipment__slot>div');
+    profileData = JSON.parse(document.querySelector('#profile-ability > script').innerText.replace(/.+\{/, '[{').replace(/;\n$/, ']'))
+    equipmentList.forEach(element => {
+        if (element.classList.length === 1 && parseInt(element.className.replace(/slot(\d+)$/, '$1')) <= 11) {
+            let gearId = element.getAttribute('data-item')
+            let bar = document.createElement('div')
+            let gearSlotQuality = parseInt(profileData[0].Equip[gearId].Element_001.value.qualityValue)
+            bar.style.width = "90%"
+            bar.style.backgroundColor = "lightgray"
+            bar.style.borderRadius = "3px"
+            bar.style.padding = "1px"
+            let quality = document.createElement('div')
+            quality.style.backgroundColor = colorPicker(gearSlotQuality)
+            quality.style.width = gearSlotQuality !== 0 ? gearSlotQuality + '%' : '100%'
+            quality.style.height = "6px"
+            quality.style.borderRadius = "3px"
+            bar.appendChild(quality)
+            element.appendChild(bar)
+            element.style.flexWrap = "wrap"
+        }
+    })
+}
+
+function colorPicker(quality) {
+    let colorId;
+    switch (true) {
+        case quality === 0:
+            colorId = 0;
+            break;
+        case quality < 39:
+            colorId = 25;
+            break;
+        case quality < 70:
+            colorId = 50;
+            break;
+        case quality < 90:
+            colorId = 70;
+            break;
+        case quality < 100:
+            colorId = 90;
+            break;
+        case quality === 100:
+            colorId = 100;
+            break;
+    }
+    return COLOR_TABLE[colorId];
+}
+
+if (settings.displayQuality)
+    displayQuality();
 
 if (settings.modifyEngravings) {
     modifyEngravingsList();
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,11 @@
 function save_options() {
     const loadAtStart = document.getElementById('loadAtStart').checked;
     const modifyEngravings = document.getElementById('modifyEngravings').checked;
+    const displayQuality = document.getElementById('displayQuality').checked;
     chrome.storage.sync.set({
         loadAtStart: loadAtStart,
-        modifyEngravings: modifyEngravings
+        modifyEngravings: modifyEngravings,
+        displayQuality: displayQuality
     }, function () {
         let status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -17,10 +19,12 @@ function restore_options() {
     // Use default value
     chrome.storage.sync.get({
         loadAtStart: true,
-        modifyEngravings: true
+        modifyEngravings: true,
+        displayQuality: true
     }, function (items) {
         document.getElementById('loadAtStart').checked = items.loadAtStart;
         document.getElementById('modifyEngravings').checked = items.modifyEngravings;
+        document.getElementById('displayQuality').checked = items.displayQuality;
     });
 }
 
